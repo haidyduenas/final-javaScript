560c82fe6bd38e88374bd3a1e520622f2e3f90eb
haidy <haidy>
2017-09-24 21:14:50 -0500

    Tabla en js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,40 @@
-// toggle class scroll 
-$(window).scroll(function() {
-    if($(this).scrollTop() > 30)
-    {
-        $('.navbar-trans').addClass('afterscroll');
-    } else
-    {
-        $('.navbar-trans').removeClass('afterscroll');
-    }  
+"use strict";
 
-});
+let seating = [];
+
+class App {
+
+  constructor (input) {
+     this.map =  input;    
+  }
+  renderTable (event) {
+  	$("#1").click(function(event) {
+    let id = event.target.id;
+    console.log(id);
+  	if(id== 1){
+	  	$('#scp-Seating').empty();
+	  	let seating = 
+	  	`<table>\
+	  	<tr>\
+	        <td >1</td><td>2</td><td >3</td><td >4</td><td >5</td>\
+	      </tr>\
+	     <tr>\
+	        <td >6</td><td >7</td><td >8</td><td >9</td><td >10</td>\
+	      </tr>\
+	  	</table>\ `
+	  	$('#formulario').append(seating);
+    	}
+  	});
+	}  	
+}
+ 
+ let classSeating = new App( seating);
+
+classSeating.renderTable();
+
+
+
+
+
+
   
